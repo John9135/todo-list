0a7f8edebbb1e7b5c87956be81bcda4809475ac8
John9135 <palacioalexander5@gmail.com>
2023-07-07 09:08:58 -0500

    requirements of project

--- index.js
+++ index.js
@@ -7,12 +7,16 @@ const port = (process.env.PORT || 3000)
 app.set('port', port)
 app.set('view engine', 'ejs')
 app.use(express.static("style"))
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
     res.render('index')
 })
+
+mongoose.connect(process.env.MONGO_DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => { console.log('conect to db') })
+    .catch((err) => console.error(err))
 
 app.listen(port, () => {
     console.log('server is running on port' + port)
